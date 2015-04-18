bdee506a8e76857bc85c5e12b44a9d339370fbd9
Sameer Mehra <sammysignal@gmail.com>
2015-04-18 16:41:52 -0400

    added Room and Dorm classes

---
+++ Dorm.py
@@ -0,0 +1,19 @@
+import Room, Student
+
+# Class that descibes a dorm.
+class Dorm:
+
+	# initializes a Dorm with the size.
+	def __init__(self, rooms, accessible=False):
+		self.rooms = rooms
+		self.accessible = False
+
+	# Gets the fitness value of the dorm based on its students
+	def get_fitness(self):
+		pass
+
+
+def run_tests():
+	pass
+
+run_tests()
---
+++ Room.py
@@ -0,0 +1,14 @@
+import Dorm, Student
+
+# Class that descibes a room in a dorm.
+class Room:
+
+	# initializes a Room with the size. student list is passed in
+	def __init__(self, size, students):
+		self.size = size
+
+
+def run_tests():
+	pass
+
+run_tests()
