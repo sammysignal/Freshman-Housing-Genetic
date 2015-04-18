f6b61d0f15bf855033e279dc5933ffc1e3eec291
Sameer Mehra <sammysignal@gmail.com>
2015-04-18 17:14:05 -0400

    wrote get_fittest function

--- Helpers.py
+++ Helpers.py
@@ -1,3 +1,4 @@
+import Dorm, Student, Room
 # Helper functions
 
 
@@ -12,5 +13,26 @@ def crossover(dorm_a, dorm_b):
 def mutate(dorm):
 	return dorm.mutate()
 
+# Gets the fittest 10% of dorm schemes in a list of
+# filled dorms. Returns items in a list.
+def get_fittest(dorm_lst):
+	if dorm_lst == []:
+		return []
+	for d in dorm_lst:
+		if not d.has_fitness:
+			# does this change the objects in the list
+			# in place?
+			d.get_fitness()
 
-def get_fittest
+	# sort list descending by fitness value
+	dorm_lst.sort(key=lambda x: x.fitness, reverse=True)
+	num = (len(dorm_lst) / 10)
+	if num == 0:
+		return dorm_lst[0]
+	else:
+		ret = []
+		for i in range(num):
+			ret.append(dorm_list[i])
+		return ret
+
+
